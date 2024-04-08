68a402170e70cecd49415c7b48412b926e436878
mauvieira <mauvieira.dev@gmail.com>
2024-04-08 10:52:11 -0300

    added error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Suspense } from "react";
 import RelayEnvironment from "./relay/RelayEnvironment";
 import Home from "./HomePage";
+import ErrorBoundary from "./ErrorBoundary";
 
 function App() {
   return (
     <RelayEnvironment>
-      <Suspense fallback={<p>loading</p>}>
-        <Home/>
-      </Suspense>
+      <ErrorBoundary fallback={<h1>Something went wrong.</h1>}>
+        <Suspense fallback={<p>loading</p>}>
+          <Home />
+        </Suspense>
+      </ErrorBoundary>
     </RelayEnvironment>
   );
 }
---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+type Props = {
+  children: React.ReactNode;
+  fallback: React.ReactNode;
+};
+
+type State = {
+  hasError: boolean;
+};
+
+class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(_: Error): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children; 
+  }
+}
+
+export default ErrorBoundary;
